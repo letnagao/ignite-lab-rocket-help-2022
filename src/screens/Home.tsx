e9876d6cf78f32475163f9a8bfed1774298153f0
letnagao <leticia-nagao@hotmail.com>
2022-07-20 23:46:36 -0300

    added navigation

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from 'react'
 import {
   HStack,
   IconButton,
@@ -8,13 +8,14 @@ import {
   Heading,
   FlatList,
   Center,
-} from 'native-base';
-import { SignOut } from 'phosphor-react-native';
-import { ChatTeardropText } from 'phosphor-react-native';
-import Logo from '../assets/logo_secondary.svg';
-import { Filter } from '../components/Filter';
-import { Order, OrderProps } from '../components/Order';
-import { Button } from '../components/Button';
+} from 'native-base'
+import { SignOut } from 'phosphor-react-native'
+import { ChatTeardropText } from 'phosphor-react-native'
+import Logo from '../assets/logo_secondary.svg'
+import { Filter } from '../components/Filter'
+import { Order, OrderProps } from '../components/Order'
+import { Button } from '../components/Button'
+import { useNavigation } from '@react-navigation/native'
 
 export function Home() {
   const [statusSelected, setStatusSelected] = useState<'open' | 'closed'>(
@@ -24,12 +25,22 @@ export function Home() {
     {
       id: '123',
       patrimony: '123456',
-      when: '18/07/2022 às 10:00',
+      when: '18/07/2022 às 14:00',
       status: 'open',
     },
   ])
 
+  const navigation = useNavigation()
   const { colors } = useTheme()
+
+  function handleNewOrder() {
+    navigation.navigate('new')
+  }
+
+  function handleOpenDetails(orderId: string) {
+    navigation.navigate('details', { orderId });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -52,8 +63,8 @@ export function Home() {
           justifyContent="space-between"
           alignItems="center"
         >
-          <Heading color="gray.100">Meus chamados</Heading>
-          <Text color="gray.200">3</Text>
+          <Heading color="gray.100">Solicitações</Heading>
+          <Text color="gray.200">{orders.length}</Text>
         </HStack>
         <HStack space={3} mb={8}>
           <Filter
@@ -72,20 +83,23 @@ export function Home() {
         <FlatList
           data={orders}
           keyExtractor={(item) => item.id}
-          renderItem={({ item }) => <Order data={item} />}
+          renderItem={({ item }) => (
+            <Order data={item} onPress={() => handleOpenDetails(item.id)} />
+          )}
           showsVerticalScrollIndicator={false}
           contentContainerStyle={{ paddingBottom: 100 }}
           ListEmptyComponent={() => (
             <Center>
-                <ChatTeardropText color={colors.gray[300]} size={40}/>
-                <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
-                    Você ainda não possui {'\n'}
-                    solicitações {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
-                </Text>
+              <ChatTeardropText color={colors.gray[300]} size={40} />
+              <Text color="gray.300" fontSize="xl" mt={6} textAlign="center">
+                Você ainda não possui {'\n'}
+                solicitações{' '}
+                {statusSelected === 'open' ? 'em andamento' : 'finalizadas'}
+              </Text>
             </Center>
           )}
         />
-        <Button title="Nova solicitação" />
+        <Button title="Nova solicitação" onPress={handleNewOrder} />
       </VStack>
     </VStack>
   )
