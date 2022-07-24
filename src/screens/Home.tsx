d446e26db58e7871a897ccbe953001af6ce9ab38
letnagao <leticia-nagao@hotmail.com>
2022-07-24 03:45:21 -0300

    added auth

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Alert } from 'react-native'
+import auth from '@react-native-firebase/auth'
 import {
   HStack,
   IconButton,
@@ -41,6 +43,15 @@ export function Home() {
     navigation.navigate('details', { orderId });
   }
 
+  function handleLogout() {
+    auth()
+      .signOut()
+      .catch(error => {
+        console.log(error);
+        return Alert.alert('Sair', 'Não foi possível sair.');
+    });
+  }
+
   return (
     <VStack flex={1} pb={6} bg="gray.700">
       <HStack
@@ -53,7 +64,10 @@ export function Home() {
         px={6}
       >
         <Logo />
-        <IconButton icon={<SignOut size={26} color={colors.gray[300]} />} />
+        <IconButton 
+          icon={<SignOut size={26} color={colors.gray[300]} />} 
+          onPress={handleLogout}
+        />
       </HStack>
       <VStack flex={1} px={6}>
         <HStack
